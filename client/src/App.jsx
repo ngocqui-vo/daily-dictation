3a79659267917474d53d24969ecddfb2dbdb4b93
qui <ngocquivo11@gmail.com>
2024-12-11 17:51:54 +0700

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lessons, setLessons] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/lesson/')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then(data => setLessons(data))
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {lessons.map(lesson => (
+        <h1>{lesson.name}</h1>
+      ))}
     </>
   )
 }
